d81c3f41eaa71e2d1120d9b59c894b89074b8cdb
knewbury <knewbury@ualberta.ca>
2019-07-12 04:47:16 +0000

    Update cp in server and run script for PBE test
    Signed-off-by: knewbury <knewbury@ualberta.ca>

--- CryptoAnalysis/src/main/java/crypto/communication/Server.java
+++ CryptoAnalysis/src/main/java/crypto/communication/Server.java
@@ -23,6 +23,7 @@ public class Server {
     //    public void start(int port) throws IOException, ParseException, ClassNotFoundException, NoSuchMethodException{
     public void start(int port) throws Exception{
 	serverSocket = new ServerSocket(port);
+	System.out.println("Starting up the server, waiting for client!");
         clientSocket = serverSocket.accept();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -41,17 +42,24 @@ public class Server {
 	    if((inputLine = in.readLine()) != null){
 		//temp trust in ourselves for passing classname as next line
 		initAnalysis(inputLine.trim());
+	    }else{
+		System.out.println("Expected to receive classname next, received nothing instead.");
 	    }
+	}else{
+	    System.out.println("The message from client is unrecognized.");
 	}
+	    
 
 	}
     }
 
     //    public void initAnalysis(String classname) throws ParseException, ClassNotFoundException, NoSuchMethodException{
     public void initAnalysis(String classname) throws Exception{
-    //TODO replace this hardcoded applicationCP
+
+	System.out.println("Initing an analysis.");
+	//TODO replace this hardcoded applicationCP
 	//as is, can only run tests from the PanathonExamples dir, as located in root
-	String[] commandLine = { "-src-prec=cache", "--rulesDir=/root/CryptoAnalysis/CryptoAnalysis/src/main/resources/JavaCryptographicArchitecture/", "--applicationCp=/root/PanathonExampleMaterials/exBin"};
+	String[] commandLine = { "-sootCp=/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/jce.jar:/root/PanathonExampleMaterials/exBin", "-src-prec=cache", "--rulesDir=/root/CryptoAnalysis/CryptoAnalysis/src/main/resources/JavaCryptographicArchitecture/", "--sootCp=/root/PanathonExampleMaterials/exBin", "--arg-class=PBEmin"};
 	System.out.println("Running test for: "+ classname);
 	System.out.println("Command line: "+ Arrays.toString(commandLine));
 
