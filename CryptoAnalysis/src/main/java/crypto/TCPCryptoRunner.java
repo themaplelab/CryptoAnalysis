cb8cea39ea07ad9009489c38237f9ea9219a813a
Kristen Newbury <knewbury@ualberta.ca>
2020-02-05 16:30:40 +0000

    Add patch adapter ssDiffTool dependency
    Dep added to pom, local build only atm
    Add extra option parsing in TCPCryptoRunner
    Add extra option parsing in Server
    Add patch adapter invoke in Server
    Fix Server sorting for classes to send,
    now uses list of class names, given from
    adapter (who knows what was gen'd), as opposed
    to just discovering things in some dir

--- CryptoAnalysis/src/main/java/crypto/TCPCryptoRunner.java
+++ CryptoAnalysis/src/main/java/crypto/TCPCryptoRunner.java
@@ -1,9 +1,11 @@
 package crypto;
 
+import java.util.Iterator;
 import crypto.communication.Server;
 import java.lang.Exception;
 import java.util.List;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -11,30 +13,48 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.ParseException;
 import crypto.analysis.CrySLRulesetSelector;
 import crypto.rules.CryptSLRule;
-
+import differ.RelaxedParser;
+import differ.SemanticOptions;
 
 public class TCPCryptoRunner {
 
     private static int port = 38400;
 	private static List<CryptSLRule> rules;
-	private static CommandLine options;
+	private static CommandLine cogniOptions;
+	private static CommandLine differOptions;
 	
     public static void main(String[] args) throws Exception{
 
-
+		RelaxedParser relaxedParser = new RelaxedParser();
+        differOptions = relaxedParser.parse(new SemanticOptions(), args);
+        List<String> options1 = new ArrayList<String>();
+		relaxedParser.getLeftovers(options1);
+		
 		//for now, TODO consider if we really want the same set of
 		//possible options as the HeadlessCryptoScanner
 		CommandLineParser parser = new DefaultParser();
-        options = parser.parse(new HeadlessCryptoScannerOptions(), args);
+        cogniOptions = parser.parse(new HeadlessCryptoScannerOptions(), options1.toArray(new String[0]));
 
 		//TODO add no option provided handling
-		if (options.hasOption("rulesDir")) {
-            String resourcesPath = options.getOptionValue("rulesDir");
+		if (cogniOptions.hasOption("rulesDir")) {
+            String resourcesPath = cogniOptions.getOptionValue("rulesDir");
             rules = CrySLRulesetSelector.makeFromPath(new File(resourcesPath));
         }
 		
 		//will automatically setup as server, no checking of args
 		Server server = new Server();
+
+		Iterator it = cogniOptions.iterator();
+		while(it.hasNext()){
+			System.out.println("cognioption: "+ it.next());
+		}
+		Iterator it2 = differOptions.iterator();
+        while(it2.hasNext()){
+	    System.out.println("differoption: "+ it2.next());
+		}
+		
+		server.setCogniOptions(cogniOptions);
+		server.setDifferOptions(differOptions);
 		server.start(port, rules);
     }
 
--- runCogniServer.sh
+++ runCogniServer.sh
@@ -1,5 +1,20 @@
+currentdir=$(pwd)
 
-java -Dcom.ibm.j9ddr.structurefile=/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/vm/j9ddr.dat  -Xshareclasses:name=Cryptotest -Djava.library.path=$LD_LIBRARY_PATH:/root/openj9-openjdk-jdk8/openj9/runtime/ddrext:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/ -cp /root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/jce.jar:/root/openj9cryptoReleases/Agent/:/root/openj9cryptoReleases/RedefExamples/target/classes/:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar/:target/sootclasses-trunk-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/ddr/classes/com/ibm/j9ddr/vm29/structure/:CryptoAnalysis/build/CryptoAnalysis-2.3-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/ddr/j9ddr.jar:/root/CryptoAnalysis/adapterOutput/ crypto.TCPCryptoRunner --rulesDir=/root/CryptoAnalysis/CryptoAnalysis/src/main/resources/JavaCryptographicArchitecture 
+#even if this dir does not exist on this machine, we need it for cp scc accessing
+projectdir="/root/eval/instagram4j/"
+originaldir=${projectdir}target/instagram4j-1.8-SNAPSHOT-jar-with-dependencies.jar
+redefdir=${projectdir}patch
+mainclass=org.brunocvcunha.instagram4j.util.InstagramHashUtil
+originalclasslist=$currentdir/originalclasses.out
 
-#/root/soot/tests/:/root/PanathonExampleMaterials/exBin/
+ssDiff="/root/ssDiffTool/target/ssDiffTool-1.0-SNAPSHOT-jar-with-dependencies.jar"
+rtjce="/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/jce.jar"
+cp=".:$ssDiff:$rtjce:${currentdir}/adapterOutput/"
+rulesDir="/root/CryptoAnalysis/CryptoAnalysis/src/main/resources/JavaCryptographicArchitecture"
+sootcp=$originaldir:.:$ssDiff:$rtjce:${currentdir}/adapterOutput/:${redefdir}
+
+java -Dcom.ibm.j9ddr.structurefile=/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/vm/j9ddr.dat  -Xshareclasses:name=Cryptotest -Djava.library.path=$LD_LIBRARY_PATH:/root/openj9-openjdk-jdk8/openj9/runtime/ddrext:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/ -cp target/sootclasses-trunk-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/ddr/classes/com/ibm/j9ddr/vm29/structure/:CryptoAnalysis/build/CryptoAnalysis-2.3-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/ddr/j9ddr.jar:$cp  crypto.TCPCryptoRunner --rulesDir=$rulesDir -sootCp=$sootcp -redefcp $redefdir -mainClass $mainclass -originalclasslist $originalclasslist
+
 #-XXjitdirectory=/root/openj9-openjdk-jdk8/objs
+
+#Does cp need: /root/openj9cryptoReleases/Agent/?
